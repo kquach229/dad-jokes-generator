9064e4bf09254b5d63dae54ef7a886e783253fee
kquach229 <kquach229>
2024-09-05 22:37:53 -0400

    style error page

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,3 +1,15 @@
+// pages/_error.tsx
 export default function ErrorPage() {
-  return <p>Sorry, something went wrong</p>;
+  return (
+    <main className='flex flex-col items-center justify-center min-h-screen bg-[#d7eaf3] p-6'>
+      <div className='bg-white p-8 rounded-lg shadow-lg max-w-sm text-center'>
+        <h1 className='text-2xl font-bold text-[#14397d] mb-4'>
+          Sorry, something went wrong
+        </h1>
+        <p className='text-[#14397d]'>
+          We encountered an unexpected error. Please try again later.
+        </p>
+      </div>
+    </main>
+  );
 }
